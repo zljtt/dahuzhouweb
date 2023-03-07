f5c1d711f5d166ececac27e37983c8a784f4b24c
ZhijianWangJohn <52527069+zljtt@users.noreply.github.com>
2023-03-07 17:03:47 -0500

    Add logger and server properties

--- src/main/java/com/zljtt/wiki/WikiApplication.java
+++ src/main/java/com/zljtt/wiki/WikiApplication.java
@@ -1,13 +1,20 @@
 package com.zljtt.wiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class WikiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WikiApplication.class, args);
-	}
+    public static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(WikiApplication.class);
+        Environment environment = application.run(args).getEnvironment();
+        LOG.info("Start successfully at address {} and port {}", environment.getProperty("server.address"), environment.getProperty("server.port"));
+    }
 
 }
