8493801dd5b67a37076a2d45b33b7e75e1e7344d
ZhijianWangJohn <52527069+zljtt@users.noreply.github.com>
2023-05-08 04:52:14 -0400

    update some release files

--- src/test/java/github/zljtt/dahuzhou/DahuzhouApplicationTests.java
+++ src/test/java/github/zljtt/dahuzhou/DahuzhouApplicationTests.java
@@ -1,13 +1,13 @@
 package github.zljtt.dahuzhou;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DahuzhouApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DahuzhouApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
